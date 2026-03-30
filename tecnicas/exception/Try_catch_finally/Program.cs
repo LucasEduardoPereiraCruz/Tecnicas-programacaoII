cd9a545b9676e82ceaf45f66f060f67999392281
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-03-30 11:05:49 -0300

    Exceções

---
+++ tecnicas/exception/Try_catch_finally/Program.cs
@@ -0,0 +1,38 @@
+﻿try
+{
+    Console.WriteLine("Digite o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine()); // Readline sempre pega como string 
+
+
+    Console.WriteLine("Digite o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    var resultado = dividendo / divisor;
+
+    Console.WriteLine($"o resultado de {dividendo}/{divisor} = {resultado}");
+}
+
+/*catch(FormatException)
+{
+    Console.WriteLine("Entre com um valor inteiro");
+}*/
+
+catch (Exception ex) when (ex.Message.Contains("format"))  
+{
+    Console.WriteLine("Entre com um valor inteiro - when: {ex.Message}");
+}
+
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Divisor não pode ser zero");
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Problema na divisão: {ex.Message}");
+}
+
+finally 
+{
+    Console.WriteLine("Acabou a divisão");
+}
---
+++ tecnicas/exception/personalizada/Program.cs
@@ -0,0 +1,69 @@
+﻿ContaBancaria conta = new ContaBancaria(300.00m);
+
+try
+{
+    conta.Sacar("100");
+}
+catch (SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Erro de saldo:" + ex.Message);
+}
+
+try
+{
+    conta.Sacar("500");
+}
+catch (SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Erro de saldo:" + ex.Message);
+}
+
+try
+{
+    conta.Sacar("asdaasda");
+}
+catch (ApplicationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public class ContaBancaria
+{
+    public decimal Saldo { get; private set; }
+
+    public ContaBancaria(decimal saldoInicial)
+    {
+        Saldo = saldoInicial;
+    }
+
+    public void Sacar(string valorTexto)
+    {
+        try
+        {
+            decimal valor = decimal.Parse(valorTexto);
+            if (valor > Saldo)
+            {
+                throw new SaldoInsuficienteException($"Saldo Insuficiente. Saldo atual: R${Saldo}. Tentativa de saque na valor de R${valor}");
+            }
+            Saldo -= valor;
+            Console.WriteLine($"Saldo atual: R${Saldo}");
+        } // fim do try
+        catch(FormatException fe)
+        {
+            throw new ApplicationException("Erro ao converter valor do saque", fe);
+        }
+        finally
+        {
+            Console.WriteLine("Fim do saque");
+        }
+    } // fim do método
+} // fim da classe 
+
+public class SaldoInsuficienteException : Exception
+{
+    public SaldoInsuficienteException() { }
+
+    public SaldoInsuficienteException(string message) : base(message) { }
+
+    public SaldoInsuficienteException(string message, Exception innerException) : base(message, innerException) { }
+}
