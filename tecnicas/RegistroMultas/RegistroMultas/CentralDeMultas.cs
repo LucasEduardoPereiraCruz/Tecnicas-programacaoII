5d581b0468cf96cd48aef7fb3da4c1eccdb5adb7
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-05-06 23:52:34 -0300

    Registro de Multas

---
+++ tecnicas/RegistroMultas/RegistroMultas/CentralDeMultas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+namespace RegistroMultas
+{
+
+    public delegate void MultaHandler(Multa m);
+
+    public class CentralDeMultas
+    {
+         // Evento 
+         public event MultaHandler MultaRegistrada;
+
+        // Lista
+        public List<Multa> multas = new List<Multa>(); // vira texto JSON
+
+        // Registrar Multa
+        public void Registrar(Multa m)
+        {
+            multas.Add(m);
+
+            Console.WriteLine("Multa Registrada");
+
+            // Dispara o evento
+            MultaRegistrada?.Invoke(m);
+        }
+
+        // Salvar em JSON
+        public void SalvarJson(string caminho)
+        {
+            string json = JsonSerializer.Serialize(multas); // Converte em Json
+
+            File.WriteAllText(caminho, json);
+        }
+
+
+        // Carregar JSON
+        public void CarregarJson(string caminho)
+        {
+            // Verifica se o arquivo realmente existe
+            if (File.Exists(caminho))
+            {
+                // Lê TODO o conteúdo do arquivo
+                // e guarda em uma string chamada json
+                string json = File.ReadAllText(caminho);
+
+
+                // Converte o texto JSON novamente para uma Lista de Multa
+                //  Deserialize = transformar texto JSON em objeto C#
+                multas = JsonSerializer.Deserialize<List<Multa>>(json); 
+            }
+        }
+    }
+}
---
+++ tecnicas/RegistroMultas/RegistroMultas/Program.cs
@@ -0,0 +1,76 @@
+﻿using RegistroMultas;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Cria a central
+        CentralDeMultas central = new CentralDeMultas();
+
+        // Conecta o evento
+        central.MultaRegistrada += VerificarMultaGrave; // Adicionar um método ao evento ----- Remover método do evento
+
+        // PRIMEIRA MULTA
+        central.Registrar(new Multa()
+        {
+            Placa = "ABC1234",
+            TipoInfracao = "Excesso de velocidade",
+            Valor = 700,
+            Data = DateTime.Now
+        });
+
+        // SEGUNDA MULTA
+        central.Registrar(new Multa()
+        {
+            Placa = "XYZ9999",
+            TipoInfracao = "Avanço de sinal",
+            Valor = 300,
+            Data = DateTime.Now
+        });
+
+        // TERCEIRA MULTA
+        central.Registrar(new Multa()
+        {
+            Placa = "JKL4567",
+            TipoInfracao = "Estacionamento proibido",
+            Valor = 150,
+            Data = DateTime.Now
+        });
+
+        // QUARTA MULTA
+        central.Registrar(new Multa()
+        {
+            Placa = "QWE8521",
+            TipoInfracao = "Dirigindo sem cinto",
+            Valor = 550,
+            Data = DateTime.Now
+        });
+
+        // Salvar JSON
+        central.SalvarJson("multas.json");
+
+        // LINQ
+        var multasGraves = central.multas
+            .Where(m => m.Valor > 500);
+
+        Console.WriteLine("\nMULTAS ACIMA DE 500:\n");
+
+        foreach (var m in multasGraves)
+        {
+            Console.WriteLine($"Placa: {m.Placa}");
+            Console.WriteLine($"Infração: {m.TipoInfracao}");
+            Console.WriteLine($"Valor: R$ {m.Valor}");
+            Console.WriteLine("-------------------");
+        }
+    }
+
+    // Método do evento
+    static void VerificarMultaGrave(Multa m)
+    {
+        if (m.Valor > 500)
+        {
+            Console.WriteLine("MULTA GRAVE DETECTADA!");
+        }
+    }
+}
