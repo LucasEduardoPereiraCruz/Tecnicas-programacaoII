ccbe391455a20ee1b0d795e862e8c52c9d076fb4
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2026-05-18 11:17:41 -0300

    ToDo

---
+++ tecnicas/TodoAqui/TodoAqui/Controllers/TarefasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAqui.Models;
+using TodoAqui.Repositories;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : Controller
+    {
+        private readonly TarefaRepository _repository;
+
+        public TarefasController(TarefaRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Tarefa>>> Get()
+        {
+            var tarefas = await _repository.ObterTodasTarefasAsync();
+            return Ok(tarefas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Tarefa novaTarefa)
+        {
+            await _repository.CriarTarefaAsync(novaTarefa);
+            return CreatedAtAction(nameof(Get), new {id = novaTarefa.Id}, novaTarefa);
+        }
+    }
+}
